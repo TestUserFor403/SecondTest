38b3cf70d3aa91e8af4f5edff42d2a878b3e281b
OgBludo <95567985+OgBludo@users.noreply.github.com>
2024-04-05 18:44:31 +0300

    Commit on init

---
+++ MFCApplication1.h
@@ -0,0 +1,50 @@
+﻿// Этот исходный код примеров MFC демонстрирует функционирование пользовательского интерфейса Fluent на основе MFC в Microsoft Office
+// ("Fluent UI") и предоставляется исключительно как справочный материал в качестве дополнения к
+// справочнику по пакету Microsoft Foundation Classes и связанной электронной документации,
+// включенной в программное обеспечение библиотеки MFC C++.
+// Условия лицензионного соглашения на копирование, использование или распространение Fluent UI доступны отдельно.
+// Для получения дополнительных сведений о нашей программе лицензирования Fluent UI посетите веб-сайт
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// (C) Корпорация Майкрософт (Microsoft Corp.)
+// Все права защищены.
+
+// MFCApplication1.h: основной файл заголовка для приложения MFCApplication1
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"       // основные символы
+
+
+// CMFCApplication1App:
+// Сведения о реализации этого класса: MFCApplication1.cpp
+//
+
+class CMFCApplication1App : public CWinAppEx
+{
+public:
+	CMFCApplication1App() noexcept;
+
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Реализация
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication1App theApp;
---
+++ MainFrm.h
@@ -0,0 +1,80 @@
+﻿// Этот исходный код MFC Samples демонстрирует функционирование пользовательского интерфейса Fluent на основе MFC в Microsoft Office
+// ("Fluent UI") и предоставляется исключительно как справочный материал в качестве дополнения к
+// справочнику по пакету Microsoft Foundation Classes и связанной электронной документации,
+// включенной в программное обеспечение библиотеки MFC C++.  
+// Условия лицензионного соглашения на копирование, использование или распространение Fluent UI доступны отдельно.  
+// Для получения дополнительных сведений о нашей лицензионной программе Fluent UI посетите веб-узел
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// (C) Корпорация Майкрософт (Microsoft Corp.)
+// Все права защищены.
+
+// MainFrm.h: интерфейс класса CMainFrame
+//
+
+#pragma once
+#include "CalendarBar.h"
+#include "Resource.h"
+
+class COutlookBar : public CMFCOutlookBar
+{
+	virtual BOOL AllowShowOnPaneMenu() const { return TRUE; }
+	virtual void GetPaneName(CString& strName) const { BOOL bNameValid = strName.LoadString(IDS_OUTLOOKBAR); ASSERT(bNameValid); if (!bNameValid) strName.Empty(); }
+};
+
+class CMainFrame : public CMDIFrameWndEx
+{
+	DECLARE_DYNAMIC(CMainFrame)
+public:
+	CMainFrame() noexcept;
+
+// Атрибуты
+public:
+
+// Операции
+public:
+
+// Переопределение
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Реализация
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:  // встроенные члены панели элементов управления
+	CMFCRibbonBar     m_wndRibbonBar;
+	CMFCRibbonApplicationButton m_MainButton;
+	CMFCToolBarImages m_PanelImages;
+	CMFCRibbonStatusBar  m_wndStatusBar;
+	COutlookBar       m_wndNavigationBar;
+	CMFCShellTreeCtrl m_wndTree;
+	CCalendarBar      m_wndCalendar;
+	CMFCCaptionBar    m_wndCaptionBar;
+
+// Созданные функции схемы сообщений
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnWindowManager();
+	afx_msg void OnApplicationLook(UINT id);
+	afx_msg void OnUpdateApplicationLook(CCmdUI* pCmdUI);
+	afx_msg void OnViewCaptionBar();
+	afx_msg void OnUpdateViewCaptionBar(CCmdUI* pCmdUI);
+	afx_msg void OnOptions();
+	DECLARE_MESSAGE_MAP()
+
+	BOOL CreateOutlookBar(CMFCOutlookBar& bar, UINT uiID, CMFCShellTreeCtrl& tree, CCalendarBar& calendar, int nInitialWidth);
+	BOOL CreateCaptionBar();
+
+	int FindFocusedOutlookWnd(CMFCOutlookBarTabCtrl** ppOutlookWnd);
+
+	CMFCOutlookBarTabCtrl* FindOutlookParent(CWnd* pWnd);
+	CMFCOutlookBarTabCtrl* m_pCurrOutlookWnd;
+	CMFCOutlookBarPane*    m_pCurrOutlookPage;
+};
+
+
